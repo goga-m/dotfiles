081f65992b5c539d5b03a66e3c9df1932fe60b7d
goga-m <goga.mamoulasvili@gmail.com>
2024-08-18 16:26:26 +0200

    feat(wezterm): add wezterm config

---
+++ dot_wezterm.lua
@@ -0,0 +1,15 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = 'Dracula (Official)'
+
+config.enable_tab_bar = false
+config.font_size = 16
+
+-- and finally, return the configuration to wezterm
+return config
