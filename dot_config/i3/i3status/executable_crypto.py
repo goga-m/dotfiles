5ab83fe9b099b42291639cb6a9ed19ef73048351
goga-m <goga.mamoulasvili@gmail.com>
2025-08-08 21:01:50 +0200

    chore: update i3 config

---
+++ dot_config/i3/i3status/executable_crypto.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import json
+import urllib.request
+
+def myround (val, r=2):
+  """
+  Converts a string of float to rounded string
+  @param {String} val, "42.551"
+  @param {int} r, the decimal to round
+  @return {string} "42.55" if r is 2
+  """
+  return "{:.{}f}".format(float(val), r)
+
+with urllib.request.urlopen("https://api.kraken.com/0/public/Ticker?pair=XXBTZEUR,EOSEUR") as url:
+    data = json.loads(url.read().decode())
+    str = "₿ " + myround(data['result']['XXBTZEUR']['c'][0]) + "€ "
+    str += "E " + myround(data['result']['EOSEUR']['c'][0]) + "€ "
+    print(str, end='')
+
