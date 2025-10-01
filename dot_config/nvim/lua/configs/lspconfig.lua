6ba963c3a8241ebe83c0a0c2907232588ab30ecc
goga-m <goga.mamoulasvili@gmail.com>
2025-10-01 09:44:57 +0200

    refactor: update lspconfig

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local configs = require "nvchad.configs.lspconfig"
 
 -- EXAMPLE
 local servers = { "html", "cssls", "ts_ls", "eslint", "pyright" }
@@ -22,8 +22,9 @@ local function set_diagnostic_virtual_text_colors()
 end
 
 -- lsps with default config
+vim.lsp.enable(servers)
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  configs[lsp] = {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
--- dot_config/nvim/lua/configs/mappings/defaults.lua
+++ dot_config/nvim/lua/configs/mappings/defaults.lua
@@ -4,6 +4,14 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "jj", "<ESC>")
 
+-- Keep cursor in place when you press * to search word under cursor
+-- map(
+--   "n", -- mode: normal
+--   "*", -- lhs
+--   ":keepjumps normal! mi*`i<CR>", -- rhs
+--   { noremap = true, silent = true }
+-- )
+
 -- Multiple copy-paste without changing the clipboard..
 map("x", "p", "pgvy", { noremap = true, silent = true, desc = "Paste and reselect pasted text" })
 
