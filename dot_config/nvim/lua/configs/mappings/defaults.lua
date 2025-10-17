48b44ebb2ae4cd072606be2b8562f3f405e87faf
goga-m <goga.mamoulasvili@gmail.com>
2025-10-17 10:06:56 +0200

    chore: update neovim

--- dot_config/nvim/lua/configs/mappings/defaults.lua
+++ dot_config/nvim/lua/configs/mappings/defaults.lua
@@ -5,12 +5,12 @@ map("i", "jk", "<ESC>")
 map("i", "jj", "<ESC>")
 
 -- Keep cursor in place when you press * to search word under cursor
--- map(
---   "n", -- mode: normal
---   "*", -- lhs
---   ":keepjumps normal! mi*`i<CR>", -- rhs
---   { noremap = true, silent = true }
--- )
+map(
+  "n", -- mode: normal
+  "*", -- lhs
+  ":keepjumps normal! mi*`i<CR>", -- rhs
+  { noremap = true, silent = true }
+)
 
 -- Multiple copy-paste without changing the clipboard..
 map("x", "p", "pgvy", { noremap = true, silent = true, desc = "Paste and reselect pasted text" })
