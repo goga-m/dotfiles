10ae9d46e91508058c7af9d14c7db3de07684c60
goga-m <goga.mamoulasvili@gmail.com>
2025-10-20 16:39:41 +0200

    feat(nvim): new mapping to opn fzf-grep with preselected text

--- dot_config/nvim/lua/configs/mappings/fzf.lua
+++ dot_config/nvim/lua/configs/mappings/fzf.lua
@@ -12,3 +12,13 @@ end, { silent = true })
 
 -- Use grep_native as it's more performant than live_grep
 vim.keymap.set("n", "<C-\\>", ':lua require"fzf-lua".live_grep_native()<CR><C-g>')
+
+-- copy the word under the cursor and search fzf live grep with the yanked text.
+vim.keymap.set("n", "**", function()
+  -- Yank the word under cursor
+  vim.cmd("normal! yiw")
+  local search_term = vim.fn.getreg('"')
+
+  -- Use fzf-lua's live_grep_native with the pre-filled search term
+  require("fzf-lua").live_grep_native({ search = search_term })
+end, { silent = true })
