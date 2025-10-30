68b471ab6eab344d9ea1a7f2157f53486674936f
goga-m <goga.mamoulasvili@gmail.com>
2025-10-30 09:42:23 +0100

    chore(nvim): add default keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jj", "<ESC>")
+map("x", "p", "pgvy", { noremap = true, silent = true, desc = "Paste and reselect pasted text" })
+
+-- Half page navigation with ctrl-k/j
+map("n", "<C-S-j>", "<C-d>")
+map("n", "<C-S-k>", "<C-u>")
